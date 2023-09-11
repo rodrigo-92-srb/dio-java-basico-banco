90947bad11cafdd78de56a42449f590e9feff1af
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-09-11 18:46:43 -0300

    Resolvendo problema

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,13 +1,34 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TODO: conhecer e importar a classe Scanner
 
-        // Exibir as mensagens para o usuário
+        Scanner scanner = new Scanner(System.in);
 
-        // Obter pela scanner os valores digitados no terminal
+        System.out.println("---------- BANCO DIO ----------");
+        System.out.println("------- CRIAÇÃO DE CONTA ------");
 
-        // Exibir a mensagem da conta criada
+        System.out.println("Por favor insira o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor insira o número da agência: ");
+        String agencia = scanner.next();
+        StringBuilder agenciaFormatada = new StringBuilder();
+        agenciaFormatada.append(agencia.substring(0, 3)).append("-");
+        agenciaFormatada.append(agencia.charAt(3));
+
+        System.out.println("Por favor insira o nome do cliente: ");
+        String cliente = scanner.next();
+
+        System.out.println("Por favor insira o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+        DecimalFormat formatoMoeda = new DecimalFormat("R$ #,##0.00");
+        String saldoFormatado = formatoMoeda.format(saldo);
+
+        System.out.println("Olá "+cliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agenciaFormatada+", conta "+conta+" e seu saldo "+saldoFormatado+" já está disponível para saque.");
+
     }
 }
