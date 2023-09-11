da8fcfa37d343ffaa9c6edbbfeebc4a34bf5eda2
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2023-09-11 18:23:23 -0300

    ajustando nome do arquivo principal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //TODO: conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o usuário
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem da conta criada
+    }
+}
